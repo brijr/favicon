33cbfc7358e6bc759f4f182f0f03cd0707d1fae7
Bridger Tower <bridgertower@gmail.com>
2024-12-11 13:00:04 -0700

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Manrope as Font } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Font({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Emoji to Favicon Converter",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
         {children}
         <Toaster />
       </body>
